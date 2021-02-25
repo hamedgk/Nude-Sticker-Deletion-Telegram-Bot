c2445ded4e30d0d7cf3168d9defe2c70d5e047c2
Hamed Gandomkar <64727357+hamedgk@users.noreply.github.com>
2021-02-26 01:50:35 +0330

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+
+use rand::Rng;
+use teloxide::prelude::*;
+use teloxide::types::File as TgFile;
+use tokio::fs::File;
+use teloxide::{requests::Request, Bot};
+use std::process::Command;
+use nude;
+use image;
+
+
+type Kindam = teloxide::types::Sticker;
+type Cx = UpdateWithCx<Message>;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting Hamed's Bot...");
+    run().await;
+}
+
+async fn run() {
+    #![allow(deprecated)]
+    let bot = Bot::new("Bot Token");
+
+    teloxide::repl(bot, |message: Cx| async move{
+        // if the message is sticker accept it!
+        match message.update.sticker() {
+            Some(sticker_struct) => {
+                
+                let Kindam {ref file_id, ..} = &sticker_struct;
+                let chat_id_str: &i64 = &message.chat_id();
+                
+                // if sticker is nude delete the message
+                if is_sticker_nude(&message.bot, &file_id, &chat_id_str).await{
+                    message.delete_message().send().await?;
+                }
+
+            },
+
+            None => (),//if the message is not sticker do nothing
+        }
+        
+
+        ResponseResult::<()>::Ok(())
+    }).await;
+    
+}
+
+async fn is_sticker_nude(_bot: &Bot, file_id_download: &String, chat_id: &i64) -> bool{
+    let random_num: u32 = rand::thread_rng().gen_range(1..10000);
+    let path = &format!("F:/NudeDatabase/{}-{}.png", &chat_id, &random_num);
+    let mut file = File::create(&path).await.unwrap();
+    let TgFile {ref file_path, .. } = _bot.get_file(file_id_download).send().await.unwrap();
+    _bot.download_file(&file_path, &mut file).await.unwrap();
+    
+    //converting downloaded sticker into png type using magick command line tool!
+    //otherwise, the nude detector can't handle the image
+    convert_to_png(&path).await;
+    let img = image::open(&path).expect("failed to open!");
+    nude::scan(&img).is_nude()
+}
+
+pub async fn convert_to_png(path: &String){
+    // this for loop is for converting unix like path to windows like, due to / and \ defference
+    // if you're using unix os, you can delete it!
+    let mut new_string = String::new();
+    for each in path.chars(){
+        if each == '/'{
+            new_string.push('\\');
+        }else {
+            new_string.push(each);
+        }
+    }
+    println!("{}", new_string);
+    Command::new("magick")
+        .arg(new_string.as_str())
+        .arg(new_string.as_str())
+        .spawn()
+        .expect("magick command failed!");
+}
